6f416c18b603e8841991da35e1ee29dc7f7b8f0d
Tasnim-Bahar-Samir <tanimsamir3@gmail.com>
2022-11-24 13:31:57 +0600

    initial setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const app = express()
+
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/',(req,res) => {
+    res.send('server is running')
+})
+
+
+app.listen(port,()=>{
+    console.log('server is running on port',port)
+})
